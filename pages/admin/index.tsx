6b0be90322cf0377ab5110b301f17ce4956e3b06
vintph16172 <vintph16172@fpt.edu.vn>
2022-07-19 13:41:00 +0700

    update

---
+++ pages/admin/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const AdminDashbroad = (props: Props) => {
+
+
+    return (
+        <div>
+            AdminDashbroad
+        </div>
+    )
+}
+
+export default AdminDashbroad
