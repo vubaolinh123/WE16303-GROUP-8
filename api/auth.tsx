a53de6eb840749e111d47cf0dd9c99d5610a4148
Vũ Quang Duy <vduy2350@gmail.com>
2022-07-31 17:49:16 +0700

    update api online

--- api/auth.tsx
+++ api/auth.tsx
@@ -2,11 +2,11 @@ import { IUser } from "../models/type";
 import instance from "./config";
 
 export const signup = (user: IUser) => {
-    return instance.post(`http://localhost:3001/api/signup`, user)   
+    return instance.post(`https://phim-poly.herokuapp.com/api/signup`, user)   
 }
 export const signin = (user: IUser) => {
-    return instance.post(`http://localhost:3001/api/signin`, user)   
+    return instance.post(`https://phim-poly.herokuapp.com/api/signin`, user)   
 }
 export const signinwithnextauth = (user: any) => {
-    return instance.post(`http://localhost:3001/api/signinwithnextauth`, user)   
+    return instance.post(`https://phim-poly.herokuapp.com/api/signinwithnextauth`, user)   
 }
