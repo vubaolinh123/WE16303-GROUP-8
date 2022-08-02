3c2b4ddcc5b535fbe855b00e61e41577e5996dfd
vintph16172 <vintph16172@fpt.edu.vn>
2022-08-02 21:41:58 +0700

    feature: AdminUser

--- api/config.tsx
+++ api/config.tsx
@@ -8,3 +8,7 @@ export const API_NodeJS = axios.create({
     baseURL: "https://nodejs-group-8.herokuapp.com/api/",
 });
 
+export const API_NodeLocal = axios.create({
+    baseURL: "http://localhost:3001/api/",
+});
+
--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import productReducer from "../features/products/products.slice";
+import userSlice from "../features/user/user.slice";
 
 
 const rootReducer = combineReducers({
     product: productReducer,
+    user: userSlice
 });
 
 export default rootReducer;
