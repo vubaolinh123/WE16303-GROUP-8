751c3af57ca751dc384e404e38d9f775b901c696
duyvq1407 <77132174+duyvq1407@users.noreply.github.com>
2022-08-04 12:26:19 +0700

    Update auth.slice.tsx

--- features/auth/auth.slice.tsx
+++ features/auth/auth.slice.tsx
@@ -1,5 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { signin, signinwithnextauth, signup } from "../../api/auth";
+import {
+  changepassword,
+  changeprofile,
+  signin,
+  signinwithnextauth,
+  signup,
+} from "../../api/auth";
 import { IUser } from "../../models/type";
 import { toast } from "react-toastify";
 import { signOut } from "next-auth/react";
@@ -7,24 +13,20 @@ import { signOut } from "next-auth/react";
 interface IUserState {
   value: { token: string; user: IUser };
   error: any;
-  isLoggedIn: boolean
+  isLoggedIn: boolean;
 }
 
 export const initialState: IUserState = {
   value: { token: "", user: { email: "", password: "" } },
   error: {},
-  isLoggedIn: false
+  isLoggedIn: false,
 };
 
 // Action
-export const login = createAsyncThunk("user/login", async (user: IUser ) => {
-  // try {
-    const { data } = await signin(user);
-    toast.success("Đăng nhập thành công");
-    return data;
-  // } catch (error: any) {
-  //   toast.error(error.response.data.message);
-  // }
+export const login = createAsyncThunk("user/login", async (user: IUser) => {
+  const { data } = await signin(user);
+  toast.success("Đăng nhập thành công");
+  return data;
 });
 
 export const loginwithnextauth = createAsyncThunk(
@@ -40,25 +42,59 @@ export const loginwithnextauth = createAsyncThunk(
   }
 );
 
+export const changepass = createAsyncThunk(
+  "user/changepass",
+  async (user: any) => {
+    const { data } = await changepassword(user);
+    toast.success("Thay đổi mật khẩu thành công");
+    return data;
+  }
+);
+
+export const changeuserprofile = createAsyncThunk(
+  "user/changeuserprofile",
+  async (user: any) => {
+    const { data } = await changeprofile(user);
+    toast.success("Thay đổi thông tin thành công");
+    return data;
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     signout: (state) => {
-      localStorage.removeItem('persist:root');
+      localStorage.removeItem("persist:root");
       signOut();
       state.isLoggedIn = false;
-      toast.success("Đăng xuất thành công")
-    }
+      toast.success("Đăng xuất thành công");
+    },
   },
   extraReducers: (builder) => {
-    builder.addCase(login.rejected, (state, action) => {
-      state.error = action.error;
-      console.log(action)
-      state.isLoggedIn = false;
-    });
     builder.addCase(login.fulfilled, (state, action) => {
       state.value = action.payload;
+      state.isLoggedIn = true;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      state.error = action.error;
+      state.isLoggedIn = false;
+    });
+    builder.addCase(changepass.fulfilled, (state, action) => {
+      state.value.user = action.payload;
+      localStorage.removeItem("persist:root");
+      state.isLoggedIn = false;
+    });
+    builder.addCase(changepass.rejected, (state, action) => {
+      state.error = action.error;
+      state.isLoggedIn = true;
+    });
+    builder.addCase(changeuserprofile.fulfilled, (state, action) => {
+      state.value.user = action.payload;
+      state.isLoggedIn = true;
+    });
+    builder.addCase(changeuserprofile.rejected, (state, action) => {
+      state.error = action.error;
       state.isLoggedIn = true;
     });
     builder.addCase(loginwithnextauth.fulfilled, (state, action) => {
