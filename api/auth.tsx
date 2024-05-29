ba033eb9460cb20403471d1b1097357b28874154
Linh Vũ <73287503+vubaolinh123@users.noreply.github.com>
2024-05-29 09:30:06 +0700

    Update auth.tsx

--- api/auth.tsx
+++ api/auth.tsx
@@ -2,10 +2,10 @@ import { IUser } from "../models/type";
 import { instance } from "./config";
 import { API_NodeJS } from "./config";
 export const signup = (user: IUser) => {
-    return API_NodeJS.post(`users/register`, user)
+    return API_NodeJS.post(`signup`, user)
 }
 export const signin = (user: IUser) => {
-    return API_NodeJS.post(`users/login`, user)
+    return API_NodeJS.post(`signin`, user)
 }
 export const signinwithnextauth = (user: any) => {
     return API_NodeJS.post(`signinwithnextauth`, user)
